5a3f6999b5134524dcfb775b91efd93929b29e5e
OnurKader <onurkader@protonmail.com>
2020-02-27 13:48:24 +0300

    Split now changes the original string

--- src/string_utils.hpp
+++ src/string_utils.hpp
@@ -9,21 +9,20 @@ void replace(char* str, char removal, char added)
 			*str = added;
 }
 
-std::vector<const char*> split(const char* str, const char* delimiter)
+std::vector<const char*> split(char* str, const char* delimiter)
 {
 	std::vector<const char*> parts;
 
-	char* dup_str = strdup(str);
-	const char* tok = strtok(dup_str, delimiter);
+	const char* tok = strtok(str, delimiter);
 	parts.push_back(tok);
 	while((tok = strtok(nullptr, delimiter)))
 		parts.push_back(tok);
 	return parts;
 }
 
-std::vector<const char*> split(const char* str, const char c)
+std::vector<const char*> split(char* str, const char c)
 {
-	const char delimiter[1] = {c};
+	const char delimiter[2] = {c, 0};
 	return split(str, delimiter);
 }
 
