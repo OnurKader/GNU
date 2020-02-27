54c3ab61bae0668968a47ff61eec0afc6ba2ca6d
OnurKader <onurkader@protonmail.com>
2020-02-27 13:48:06 +0300

    Whoops, fixed a small issue

--- src/test.cpp
+++ src/test.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
 	(void)argc;
 	(void)argv;
 
-	const char* str = "Hello World!! My name is Onur!! Yeah!!Science Bitch!";
+	char str[] = "Hello World!! My name is Onur!! Yeah!!Science Bitch!";
 	std::vector<const char*> words = split(str, "! ");
 	for(auto& word: words)
 	{
